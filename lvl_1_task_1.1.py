6ee8189de38d6fd3f215bfab755030b800bc767d
udalovps <udalovpavel95@gmail.com>
2023-05-06 11:09:31 +0500

    complete task 1.1

---
+++ lvl_1_task_1.1.py
@@ -0,0 +1,20 @@
+my_favorite_songs = 'Waste a Moment, Staying\' Alive, A Sorta Fairytale, Start Me Up, New Salvation'
+
+#x = [i for i, ltr in enumerate(my_favorite_songs) if ltr == "t"]
+#print(x) 
+
+#y = [i for i, ltr in enumerate(my_favorite_songs) if ltr == "S"]
+#print(y)
+
+#z = [i for i, ltr in enumerate(my_favorite_songs) if ltr == "e"]
+#print(z)
+
+#for i in enumerate(my_favorite_songs):
+  #print(i)
+
+#print(my_favorite_songs.index("S"))
+
+print(my_favorite_songs[0:14])
+print(my_favorite_songs[-13:])
+print(my_favorite_songs[16:30])
+print(my_favorite_songs[-26:-15])
