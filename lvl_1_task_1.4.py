159c8093112724d3780d01b9caab15f1058b0517
udalovps <udalovpavel95@gmail.com>
2023-05-06 17:32:31 +0500

    files added

---
+++ lvl_1_task_1.4.py
@@ -0,0 +1,32 @@
+titles = {
+    'Кроссовки тип 3 (Adidas)': '100000110',
+    'Мячик тип 2 (Adidas)': '100000146',
+    'Кепка тип 1 (Adidas)': '100000149',
+    'Ремень тип 2 (Nike)': '100000194',
+    'Футболка тип 1 (Adidas)': '100000224',
+    'Шапка тип 5 (Puma)': '100000280',
+}
+
+store = {
+    '100000110': [{'quantity': 31, 'price': 1637}],
+    '100000146': [ {'quantity': 4, 'price': 45}, {'quantity': 10, 'price': 48}],
+    '100000149': [ {'quantity': 28, 'price': 279}, {'quantity': 32, 'price': 291}],
+    '100000194': [{'quantity': 8, 'price': 220}, {'quantity': 1, 'price': 170}],
+    '100000224': [{'quantity': 61, 'price': 438}, {'quantity': 23, 'price': 302},  {'quantity': 50, 'price': 412}],
+    '100000280': [{'quantity': 26, 'price': 175}, ]
+}
+
+for key in titles.keys():
+  title_value = titles[key]
+  store_value = store[title_value]
+
+  sum = 0
+  for qp in store_value:
+    quantity = qp['quantity']
+    price = qp['price']
+    sum += quantity * price
+
+  #print(store_value)
+  #print(qp)
+
+  print(key, "-", quantity, "шт", sum,  "руб")
